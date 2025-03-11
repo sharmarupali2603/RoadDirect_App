655d7fbfb2965d16868904255d62a328bbaaddf8
Daniel Stocker <development@roaddirect.co.nz>
2025-03-11 20:55:40 +1300

    config tweaks for my local environment

--- src/app/Services/Client/client.service.ts
+++ src/app/Services/Client/client.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { DatabaseService } from '../Database/database.service';
+import { Injectable } from '@angular/core';
 import { Observable, from } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { DatabaseService } from '../Database/database.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
   private mainUrl =
-    'https://fabricate.mockaroo.com/api/v1/databases/eboard/api/';
+    'https://pwa.roaddrectdaniel/api/ClientManagement/';
   private token = '76fda9e0-486a-4efc-b4ee-3ea32d774c8c'; // Replace with your actual token
 
   // Function to get headers with Bearer token
   private getHeaders(): HttpHeaders {
     return new HttpHeaders({
-      Authorization: `Bearer ${this.token}`,
+      //Authorization: `Bearer ${this.token}`,
       'Content-Type': 'application/json',
     });
   }
--- src/app/Services/Jobs/jobs.service.ts
+++ src/app/Services/Jobs/jobs.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { catchError, from, map, Observable, tap } from 'rxjs';
 import { DatabaseService } from '../Database/database.service';
 
@@ -8,28 +8,28 @@ import { DatabaseService } from '../Database/database.service';
 })
 export class JobsService {
   private mainUrl =
-    'https://fabricate.mockaroo.com/api/v1/databases/eboard/api/';
+    'https://pwa.roaddrectdaniel/';
   private token = '76fda9e0-486a-4efc-b4ee-3ea32d774c8c'; // Replace with your actual token
 
   constructor(private http: HttpClient, private db:DatabaseService) {}
   // Function to get headers with Bearer token
   private getHeaders(): HttpHeaders {
     return new HttpHeaders({
-      Authorization: `Bearer ${this.token}`,
+      //Authorization: `Bearer ${this.token}`,
       'Content-Type': 'application/json',
     });
   }
 
   // GET request
   getCurrentUser(): Observable<any> {
-    return this.http.get(this.mainUrl + 'GetCurrentUser', {
+    return this.http.get(this.mainUrl + 'api/UserManagement/GetCurrentUser', {
       headers: this.getHeaders(),
     });
   }
 
   // POST request with Bearer token
   getJobsByDateRange(postData: any): Observable<any> {
-    return this.http.post(this.mainUrl + 'GetJobsByDateRange', postData, {
+    return this.http.post(this.mainUrl + 'api/Job/GetJobsByDateRange', postData, {
       headers: this.getHeaders(),
     }).pipe(
           tap((response) => {
