ca117b0ca4677a020d82ade8eb2d92398458fcc1
Daniel Stocker <development@roaddirect.co.nz>
2025-04-23 14:11:11 +1200

    css tweaks and tidies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateRecordTaskComponent } from './Page/create-record-task/create-record-task.component';
 import { DashboardComponent } from './Page/dashboard/dashboard.component';
+import { EditEquipmentComponent } from './Page/edit-equipment/edit-equipment.component';
+import { EditTimeComponent } from './Page/edit-time/edit-time.component';
 import { JobExpandComponent } from './Page/job-expand/job-expand.component';
 import { LocationComponent } from './Page/location/location.component';
 import { TasksComponent } from './Page/tasks/tasks.component';
-import { EditTimeComponent } from './Page/edit-time/edit-time.component';
-import { EditEquipmentComponent } from './Page/edit-equipment/edit-equipment.component';
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', redirectTo: '' },
@@ -14,8 +14,8 @@ const routes: Routes = [
   { path: 'tasks', component: TasksComponent },
   { path: 'location', component: LocationComponent },
   { path: 'create-record-task', component: CreateRecordTaskComponent },
-  {path: 'edit-time', component: EditTimeComponent},
-  {path: 'edit-equipment', component: EditEquipmentComponent},
+  { path: 'edit-time', component: EditTimeComponent },
+  { path: 'edit-equipment', component: EditEquipmentComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
